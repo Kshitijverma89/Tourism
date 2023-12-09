63fa036cb3dd4e0dcbb85992ec244bca73daf4a7
trivia-sandyc4136 <sandyc4136@gmail.com>
2023-12-10 02:56:57 +0545

    added bookings, reviews

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,20 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import tourRoute from "./routes/tours.js"
+import tourRoute from "./routes/tours.js";
 import userRoute from "./routes/users.js";
 import authRoute from "./routes/auth.js";
+import reviewRoute from "./routes/reviews.js";
+import bookingRoute from "./routes/bookings.js";
 
 dotenv.config()
 const app = express();
 const port = process.env.PORT || 8000;
+const corsOption={
+    origin: true,
+    Credentials: true
+}
+
 
 //database connection
 
@@ -35,13 +42,15 @@ app.get('/', (req,res)=>{
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOption));
 app.use(cookieParser());
-app.use('/tours', tourRoute);
-app.use('/tours', tourRoute);
-app.use('/users', userRoute);
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/tours', tourRoute);
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/review', reviewRoute);
+app.use('/api/v1/booking', bookingRoute);
 
 app.listen(port, ()=>{
-    connect();
-    console.log("server listening on port", port)
+    connect(); 
+    console.log("server listening on port", port);
 });
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,22 +1,30 @@
 
 import express from 'express';
-import {createTour, updateTour, deleteTour, getSingleTour, getAllTour} from "./../controllers/tourController.js";
+import {createTour, updateTour, deleteTour, getSingleTour, getAllTour, getTourBySearch, getFeaturedTour, getTourCount} from "./../controllers/tourController.js";
+
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router= express.Router();
 
 //create new tour
-router.post("/", createTour);
+router.post("/",verifyAdmin, createTour);
 
-//update new tour
-router.put("/:id", updateTour);
+//update  tour
+router.put("/:id", verifyAdmin, updateTour);
 
-//delete new tour
-router.delete("/:id", deleteTour);
+//delete  tour
+router.delete("/:id",verifyAdmin,  deleteTour);
 
-//get Single new tour
+//get Single  tour
 router.get("/:id", getSingleTour);
 
-//create new tour
+// get all tours
 router.get("/", getAllTour);
 
-export default router;
+//get  tour by search
+router.get("/search/getTourBySearch", getTourBySearch);
+
+router.get("/search/getFeaturedTours", getFeaturedTour);
+router.get("/search/getTourCount", getTourCount);
+
+export default router; 
