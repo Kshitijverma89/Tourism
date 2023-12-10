ada9f13a005cee5bdb3f93379bf6004de3ab5a1c
Kshitij90 <kshitijverma89@gmail.com>
2023-12-10 15:57:18 +0530

    fetch api

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import {createTour, updateTour, deleteTour, getSingleTour, getAllTour} from "./../controllers/tourController.js";
+import {createTour, updateTour, deleteTour, getSingleTour, getAllTour,  getTourBySearch, getFeaturedTour, getTourCount} from "./../controllers/tourController.js";
 import {verifyAdmin} from "../utils/verifyToken.js"
 
 const router= express.Router();
@@ -19,5 +19,12 @@ router.get("/:id", getSingleTour);
 
 //create new tour
 router.get("/", getAllTour);
+
+//get tour by search
+router.get("/search/getTourBySearch", getTourBySearch);
+
+router.get("/search/getFeaturedTours", getFeaturedTour);
+
+router.get("/search/getTourCount", getTourCount);
 
 export default router;
--- frontend/src/Components/FeaturedTourList/Featured-tours.jsx
+++ frontend/src/Components/FeaturedTourList/Featured-tours.jsx
@@ -2,13 +2,20 @@ import React from "react"
 import TourCard from "../../Shared/TourCards";
 import tourData from "../../assets/data/tours";
 import {Col} from "reactstrap";
+import useFetch from "../../hooks/useFetch";
+import { BASE_URL } from "../../utils/config";
 
 const FeaturedTourList =()=>{
+
+    const {data: featuredTours} = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
+
+    console.log(featuredTours);
+
     return(
         <>
         {
-            tourData?.map(tour=>(
-                <Col lg="3" className="mb-4" key={tour.id}><TourCard tour={tour}/></Col>
+            featuredTours?.map(tour=>(
+                <Col lg="3" className="mb-4" key={tour._id}><TourCard tour={tour}/></Col>
             ))
         }
         </>
